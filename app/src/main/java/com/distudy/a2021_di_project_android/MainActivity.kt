389814778a2033b8db9cd87437a911659c897b9e
KimSeongHeon <kim.seongheon@worksmobile.com>
2021-05-31 23:13:12 +0900

    Initial commit

---
+++ app/src/main/java/com/distudy/a2021_di_project_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.distudy.a2021_di_project_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
