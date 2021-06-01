d194473f7ad9f38d028165286c492b20f2e2a4d5
KimSeongHeon <47879772+KimSeongHeon@users.noreply.github.com>
2021-06-01 19:17:07 +0900

    feat : fragment 추가 및 activity에 임의적으로 추가

--- app/src/main/java/com/distudy/a2021_di_project_android/MainActivity.kt
+++ app/src/main/java/com/distudy/a2021_di_project_android/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.distudy.a2021_di_project_android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.distudy.a2021_di_project_android.ui.UserListFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().add(R.id.frame_content, UserListFragment())
+        }
     }
 }
--- app/src/main/java/com/distudy/a2021_di_project_android/ui/UserListFragment.kt
+++ app/src/main/java/com/distudy/a2021_di_project_android/ui/UserListFragment.kt
@@ -1,13 +1,30 @@
 package com.distudy.a2021_di_project_android.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.distudy.a2021_di_project_android.viewModel.UserListViewModel
 import androidx.fragment.app.viewModels
+import com.distudy.a2021_di_project_android.viewModel.UserListViewModel
 
 class UserListFragment: Fragment() {
+    private lateinit var binding: fragmentUserListBinding
     private val viewModel: UserListViewModel by viewModels()
 
     companion object {
         private const val TAG = "UserListFragment"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    fun initViewModel() {
+
     }
 }
